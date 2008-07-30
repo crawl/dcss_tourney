99c9dd25304a41086d19e31277a17172839bf8dc
crawl <crawl@42476179-f906-4ffd-81fe-f88aabc8fb63>
2008-07-30 16:39:03 +0000

    r903@crawl:  crawl | 2008-07-30 12:38:56 -0400  Some more of this... no db code, but the string munging code is actually  in place now. I'm going to put the rest of it in place but will need help  with the db munging.
    git-svn-id: svn+ssh://crawl.akrasiac.org/usr/local/crawlsvn@19 42476179-f906-4ffd-81fe-f88aabc8fb63

--- update-page.py
+++ update-page.py
@@ -4,48 +4,67 @@
 # Right now I am just going to pseudocode the basic logic,
 # because I am not actually fluent in Python.
 
-# "print" should be "write to file with an implicit newline" I think
-# maybe write a function for that, maybe python does it automagically
-# I don't even know
+page = "" # aw yeah global variables all up in this I am going to hell
+          # HELL i tell you
+headers = "<html><head><title>Crawl Tournament Leaderboard 2008</title></head><body>\n"
+footers = "</body></html>"
 
 def update_scoring_page(db):
   """Master function!"""
-  open up the file to write to
-  print headers
-  print "<table border=1>"
-  print "<tr>"
-  print "<td>Leading Players"
-  leading_players_line = get_top_players_in(db)
-  print leading_players_line
-  print "</td>"
-  print "<td>Leading Clans"
-  leading_clans_line = get_top_clans_in(db)
-  print "</td>"
-  print "<td>First Victory"
-  first_victory_line = get_first_victories_in(db)
-  print "</td>"
-  print "</tr>"
-  print "<tr>"
-  ...
+  add_to_page(headers)
+  add_to_page("<table border=1>")
+  add_to_page("<tr>")
+  add_to_page("<td>Leading Players")
+  leading_players_line = format_line(get_top_players_in(db), player, points)
+  add_to_page(leading_players_line)
+  add_to_page("</td>")
+  add_to_page("<td>Leading Clans")
+  leading_clans_line = format_line(get_top_clans_in(db), clan, points)
+  add_to_page(leading_clans_line)
+  add_to_page("</td>")
+  add_to_page("<td>First Victory")
+  first_victory_line = format_line(get_first_victories_in(db), player, end_time)
+  add_to_page("</td>")
+  add_to_page("</tr>")
+  add_to_page("<tr>")
+  add_to_page("<td>Fastest Realtime Win")
+  fastest_realtime_line = format_line(get_fastest_realtime_wins_in(db), player, duration)
+  add_to_page(fastest_realtime_line)
+  add_to_page("</td>")
+  add_to_page("<td>Fastest Turncount Win")
+  fastest_turncount_line = format_line(get_fastest_turncount_wins_in(db), player, duration)
+  add_to_page(fastest_turncount_line)
+  add_to_page("</td>")
+  add_to_page("<td>First All-Runer")
+  first_allruner_line = format_line(get_first_allruners_in(db), player, end_time)
+  add_to_page(first_allruner_line)
+  add_to_page("</td>")
+  add_to_page("</tr>")
+  add_to_page("<tr>")
 
 
 
 def get_top_players_in(db):
-  """get the top three players in the db and make a line for them in
-  the front page. right now we just return a sample because I don't know
-  how to do this."""
-  return "<ul><li>Player1 : score</li><li>Player2: score</li><li>Player3 : score</li></ul>"
+  """get the top three players in the db by points"""
 
 def get_top_players_in(db):
-  """get the top three clans in the db and make a line for them in
-  the front page. right now we just return a sample because I don't know
-  how to do this."""
-  return "<ul><li>Clan1 : score</li><li>Clan2 : score</li><li>Clan3 : score</li></ul>"
+  """get the top three clans in the db""" 
 
 def get_first_victories_in(db):
   """yeah"""
-  return "<ul><li>Player1 : time</li><li>Player2 : time</li><li>Player3 : time</li></ul>"
 
 ...
 
-    
+def format_line(data, thing, sortindex):
+  """Make a line of the appropriate sort for the thing and sortindex.
+  Currently assumes we get things in order..."""
+  my_line = "<ul>" 
+  for record in data:
+    my_line= "<li>" + my_line + thing + " : " + sortindex + "</li>"
+  my_line = my_line + "</ul>"
+  return my_line
+
+def add_to_page(string):
+  """put together a page! yeah"""
+  page = page + string + "\n"
+  return
