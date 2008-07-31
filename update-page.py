b3793c58d5581bc5df3bcd1ff15b91480c9356b2
crawl <crawl@42476179-f906-4ffd-81fe-f88aabc8fb63>
2008-07-31 01:15:00 +0000

    r925@crawl:  crawl | 2008-07-30 21:14:31 -0400  At Violet's suggestion, make a new_table_item function.  Make a stub function for every calculation we need to do.  Fill out the template.  In theory this would work with all the calculations put in, but  in practice    it doesn't write to a file and its sorting is nonexistent.
    git-svn-id: svn+ssh://crawl.akrasiac.org/usr/local/crawlsvn@32 42476179-f906-4ffd-81fe-f88aabc8fb63

--- update-page.py
+++ update-page.py
@@ -14,36 +14,30 @@ def update_scoring_page(db):
   add_to_page(headers)
   add_to_page("<table border=1>")
   add_to_page("<tr>")
-  add_to_page("<td>Leading Players")
-  leading_players_line = format_line(get_top_players_in(db), player, points)
-  add_to_page(leading_players_line)
-  add_to_page("</td>")
-  add_to_page("<td>Leading Clans")
-  leading_clans_line = format_line(get_top_clans_in(db), clan, points)
-  add_to_page(leading_clans_line)
-  add_to_page("</td>")
-  add_to_page("<td>First Victory")
-  first_victory_line = format_line(get_first_victories_in(db), player, end_time)
-  add_to_page("</td>")
+  new_table_element("Leading Players", format_line(get_top_players_in(db), player, points))
+  new_table_element("Leading Clans", format_line(get_top_clans_in(db), clan, points))
+  new_table_element("First Victory", format_line(get_first_victories_in(db), player, end_time))
   add_to_page("</tr>")
   add_to_page("<tr>")
-  add_to_page("<td>Fastest Realtime Win")
-  fastest_realtime_line = format_line(get_fastest_realtime_wins_in(db), player, duration)
-  add_to_page(fastest_realtime_line)
-  add_to_page("</td>")
-  add_to_page("<td>Fastest Turncount Win")
-  fastest_turncount_line = format_line(get_fastest_turncount_wins_in(db), player, duration)
-  add_to_page(fastest_turncount_line)
-  add_to_page("</td>")
-  add_to_page("<td>First All-Runer")
-  first_allruner_line = format_line(get_first_allruners_in(db), player, end_time)
-  add_to_page(first_allruner_line)
-  add_to_page("</td>")
+  new_table_element("Fastest Realtime Win", format_line(get_fastest_realtime_wins_in(db), player, duration))
+  new_table_element("Fastest Turncount Win", format_line(get_fastest_turncount_wins_in(db), player, duration))
+  new_table_element("First All-Rune Win", format_line(get_first_allruners_in(db), player, end_time))
   add_to_page("</tr>")
   add_to_page("<tr>")
-
-
-
+  new_table_element("Most Uniques Killed", format_line(most_uniques_killed_in(db), player, number))
+  new_table_element("Most High Scores", format_line(most_high_scores_in(db), player, number))
+  new_table_element("Lowest DL at XL 1", format_line(lowest_dl_in(db), player, dl))
+  add_to_page("</tr>")
+  add_to_page("<tr>")
+  new_table_element("Most Uniques Killed: Clan", format_line(clan_most_uniques_killed_in(db), clan, number))
+  new_table_element("Most High Scores: Clan", format_line(clan_most_high_scores_in(db), clan, number))
+  new_table_element("Longest Streak", format_line(longest_streak_in(db), player, number))
+  add_to_page("</tr>")
+  add_to_page("</table><br><br>")
+  add_to_page("Lowest High Scores:")
+  add_to_page(format_line(lowest_high_scores_in(db), raceclass, score))
+  add_to_page(footers)
+  
 def get_top_players_in(db):
   """get the top three players in the db by points"""
 
@@ -51,13 +45,45 @@ def get_top_clans_in(db):
   """get the top three clans in the db""" 
 
 def get_first_victories_in(db):
-  """yeah"""
+  """get the first three victories in the db"""
 
-...
+def get_fastest_realtime_wins_in(db):
+  """get the three fastest realtime wins in the db"""
+
+def get_fastest_turncount_wins_in(db):
+  """get the three fastest turncount wins in the db"""
+
+def get_first_allruners_in(db):
+  """get the first three all-rune victories in the db"""
+
+def most_uniques_killed_in(db):
+  """get the three players who have killed the most uniques"""
+
+def most_high_scores_in(db):
+  """get the three players with the most high scores"""
+
+def lowest_dl_in(db):
+  """get the three characters with the lowest xl1 dl"""
+
+def clan_most_uniques_killed(db):
+  """get the three clans with the most uniques killed"""
+
+def clan_most_high_scores(db):
+  """get the three clans with the most high scores"""
+
+def longest_streak_in(db):
+  """get the three players with the longest streaks"""
+
+def lowest_high_scores_in(db):
+  """get the ten or so lowest high scores of any race/class combo"""
+
+def new_table_element(string, function):
+  add_to_page("<td>" + string + "\n" + function + "</td>")
 
 def format_line(data, thing, sortindex):
   """Make a line of the appropriate sort for the thing and sortindex.
-  Currently assumes we get things in order..."""
+  Currently assumes we get things in order... and that sortindex works...
+  which is a totally bogus assumption"""
   my_line = "<ul>" 
   for record in data:
     my_line= "<li>" + my_line + thing + " : " + sortindex + "</li>"
