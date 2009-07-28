e33c3b786a4eb38e4429717e521deba024207f64
crawl <crawl@42476179-f906-4ffd-81fe-f88aabc8fb63>
2009-07-28 19:41:51 +0000

    Remove use of 2.5-specific function
    git-svn-id: svn+ssh://crawl.akrasiac.org/usr/local/crawlsvn@214 42476179-f906-4ffd-81fe-f88aabc8fb63

--- nemchoice.py
+++ nemchoice.py
@@ -2,6 +2,7 @@
 
 import nominate_combo
 from datetime import datetime
+import time
 
 def _fixup_nominee_validity(c):
   newcombos = []
@@ -20,7 +21,7 @@ NEMELEX_SET = set([x[0] for x in NEMELEX_COMBOS])
 def _to_date(when):
   if when.endswith('D') or when.endswith('S'):
     when = when[:-1]
-  return datetime.strptime(when, '%Y%m%d%H%M%S')
+  return datetime(*(time.strptime(when, '%Y%m%d%H%M%S')[0:6]))
 
 def current_nemelex_choice():
   return NEMELEX_COMBOS and NEMELEX_COMBOS[-1]
@@ -31,9 +32,9 @@ def previous_nemelex_choices():
 def is_nemelex_choice(combo, when):
   """Returns true if the given combo for a game that ended at the given
   datetime is a chosen combo for the Nemelex' Choice banner."""
-  if isinstance(when, str) or isinstance(when, unicode):
-    when = _to_date(when)
   if combo in NEMELEX_SET:
+    if isinstance(when, str) or isinstance(when, unicode):
+      when = _to_date(when)
     for c in NEMELEX_COMBOS:
       if c[0] == combo and when >= c[1] and (not c[2] or when < c[2]):
         return True
