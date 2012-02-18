99da6d046741501deba98b939effdf3770ad695a
elliptic <hyperelliptical@gmail.com>
2012-02-18 14:17:59 -0500

    Add forgotten new file.

---
+++ test_data.py
@@ -0,0 +1,17 @@
+from crawl_utils import LOCAL_TEST
+CDO = 'http://crawl.develz.org/'
+CAO = 'http://crawl.akrasiac.org/'
+
+USE_TEST = False
+
+TEST_YEAR = USE_TEST and '2012'
+TEST_VERSION = USE_TEST and '0.11'
+TEST_START_TIME = USE_TEST and (TEST_YEAR + '0126')
+TEST_END_TIME   = USE_TEST and (TEST_YEAR + '0312')
+TEST_HARE_START_TIME = USE_TEST and (TEST_YEAR + '0311')
+TEST_LOGS = USE_TEST and [ LOCAL_TEST and ('cao-logfile-git', CAO + 'logfile-git')
+         or 'cao-logfile-git',
+         ('cdo-logfile-svn', CDO + 'allgames-svn.txt') ]
+TEST_MILESTONES = USE_TEST and [ LOCAL_TEST and ('cao-milestones-git', CAO + 'milestones-git')
+               or 'cao-milestones-git',
+               ('cdo-milestones-svn', CDO + 'milestones-svn.txt') ]
