a9a550f7ad1918e07c5006cf3041f61e33f9068e
elliptic <hyperelliptical@gmail.com>
2012-08-27 00:34:39 -0400

    0.11 unique changes.

--- uniq.py
+++ uniq.py
@@ -1,7 +1,7 @@
 UNIQUES = [ "Ijyb", "Blork", "Urug", "Erolcha", "Snorg",
             "Antaeus", "Xtahua", "Tiamat", "Boris", "Murray", "Terence",
             "Jessica", "Sigmund", "Edmund", "Psyche", "Donald",
-            "Joseph", "Erica", "Josephine", "Harold", "Jozef",
+            "Joseph", "Erica", "Josephine", "Harold",
             "Agnes", "Maud", "Louise", "Frances", "Rupert", "Wiglaf",
             "Norris", "Frederick", "Margery", "Mnoleg", "Lom Lobon",
             "Cerebov", "Gloorx Vloq", "Geryon", "Dispater", "Asmodeus",
@@ -12,13 +12,13 @@ UNIQUES = [ "Ijyb", "Blork", "Urug", "Erolcha", "Snorg",
             "Grum", "Crazy Yiuf", "Gastronok", "Pikel", "Menkaure",
             "Khufu", "Ilsuiw", "Aizul", "Mara", "Purgy", "Grinder",
             "Kirke", "Nikola", "Maurice", "Mennas", "Jory", 
-            "the Serpent of Hell", "the Enchantress", "Ignacio" ]
+            "the Serpent of Hell", "the Enchantress", "Ignacio", "Arachne", "Fannar" ]
 
 HARD_UNIQUES = [ "Antaeus", "Asmodeus", "Boris", "Cerebov", "Dispater", "Dissolution", "Ereshkigal", "Frederick", "Geryon", "Gloorx Vloq", "Ignacio", "Ilsuiw", "Jory", "Khufu", "Lom Lobon", "Mara", "Margery", "Mennas", "Mnoleg", "Murray", "the Enchantress", "the Lernaean hydra", "the Serpent of Hell", "the royal jelly", "Tiamat", "Xtahua" ]
 
-MEDIUM_UNIQUES = [ "Agnes", "Aizul", "Azrael", "Donald", "Frances", "Kirke", "Louise", "Maud", "Nergalle", "Nessos", "Nikola", "Norris", "Polyphemus", "Roxanne", "Rupert", "Saint Roka", "Snorg", "Wiglaf" ]
+MEDIUM_UNIQUES = [ "Agnes", "Aizul", "Arachne", "Azrael", "Donald", "Frances", "Kirke", "Louise", "Maud", "Nergalle", "Nessos", "Nikola", "Norris", "Polyphemus", "Roxanne", "Rupert", "Saint Roka", "Snorg", "Wiglaf" ]
 
-EASY_UNIQUES = [ "Blork", "Crazy Yiuf", "Dowan", "Duvessa", "Edmund", "Erica", "Erolcha", "Eustachio", "Gastronok", "Grum", "Grinder", "Harold", "Ijyb", "Jessica", "Joseph", "Josephine", "Jozef", "Maurice", "Menkaure", "Pikel", "Prince Ribbit", "Psyche", "Purgy", "Sigmund", "Sonja", "Terence", "Urug" ]
+EASY_UNIQUES = [ "Blork", "Crazy Yiuf", "Dowan", "Duvessa", "Edmund", "Erica", "Erolcha", "Eustachio", "Fannar", "Gastronok", "Grum", "Grinder", "Harold", "Ijyb", "Jessica", "Joseph", "Josephine", "Maurice", "Menkaure", "Pikel", "Prince Ribbit", "Psyche", "Purgy", "Sigmund", "Sonja", "Terence", "Urug" ]
 
 UNIQ_SET = set(UNIQUES)
 
