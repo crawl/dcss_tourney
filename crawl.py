93b511e18cec458cfb2b1bab7c25b9041e815fba
elliptic <hyperelliptical@gmail.com>
2014-04-03 20:33:37 -0400

    Add Dith to the list of gods.

--- crawl.py
+++ crawl.py
@@ -5,7 +5,7 @@ NRUNES = 17
 GODS = ['No God', 'Zin', 'the Shining One', 'Kikubaaqudgha',
         'Yredelemnul', 'Xom', 'Vehumet', 'Okawaru', 'Makhleb',
         'Sif Muna', 'Trog', 'Nemelex Xobeh', 'Elyvilon', 'Lugonu',
-        'Beogh', 'Fedhas', 'Jiyva', 'Cheibriados', 'Ashenzari']
+        'Beogh', 'Fedhas', 'Jiyva', 'Cheibriados', 'Ashenzari', 'Dithmenos']
 
 MAGIC_SKILLS = ['Spellcasting', 'Conjurations', 'Hexes', 'Charms',
                 'Summonings', 'Necromancy', 'Translocations',
