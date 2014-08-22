746ed0364ff9618b9f0be7f6d3488b20010b2d52
elliptic <hyperelliptical@gmail.com>
2014-08-22 13:55:06 -0400

    Qazlal exists now.

--- crawl.py
+++ crawl.py
@@ -5,7 +5,8 @@ NRUNES = 17
 GODS = ['No God', 'Zin', 'the Shining One', 'Kikubaaqudgha',
         'Yredelemnul', 'Xom', 'Vehumet', 'Okawaru', 'Makhleb',
         'Sif Muna', 'Trog', 'Nemelex Xobeh', 'Elyvilon', 'Lugonu',
-        'Beogh', 'Fedhas', 'Jiyva', 'Cheibriados', 'Ashenzari', 'Dithmenos']
+        'Beogh', 'Fedhas', 'Jiyva', 'Cheibriados', 'Ashenzari', 'Dithmenos',
+        'Qazlal']
 
 MAGIC_SKILLS = ['Spellcasting', 'Conjurations', 'Hexes', 'Charms',
                 'Summonings', 'Necromancy', 'Translocations',
