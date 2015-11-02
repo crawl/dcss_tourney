70745c64f280801286c4e6ea85d1edb86cde603e
elliptic <hyperelliptical@gmail.com>
2015-11-01 22:05:55 -0500

    Disable servers in test_data.py if they don't have 0.17 up yet.
    To run the scripts on the test dates, change USE_TEST to True in
    test_data.py.

--- test_data.py
+++ test_data.py
@@ -21,24 +21,24 @@ TEST_CLAN_DEADLINE = (USE_TEST and
 TEST_HARE_START_TIME = USE_TEST and (TEST_YEAR + '1101')
 TEST_LOGS = USE_TEST and [
          ('cao-logfile-0.17', CAO + 'logfile17'),
-         ('cdo-logfile-0.17', CDO + 'allgames-0.17.txt'),
-         ('cue-logfile-0.17', CUE + 'meta/0.17/logfile'),
+         #('cdo-logfile-0.17', CDO + 'allgames-0.17.txt'),
+         #('cue-logfile-0.17', CUE + 'meta/0.17/logfile'),
          ('cbro-logfile-0.17', CBRO + 'meta/0.17/logfile'),
-         ('cpo-logfile-0.17', CPO + 'dcss-logfiles-0.17'),
-         ('cwz-logfile-0.17', CWZ + '0.17/logfile'),
-         ('cxc-logfile-0.17', CXC + 'meta/0.17/logfile'),
+         #('cpo-logfile-0.17', CPO + 'dcss-logfiles-0.17'),
+         #('cwz-logfile-0.17', CWZ + '0.17/logfile'),
+         #('cxc-logfile-0.17', CXC + 'meta/0.17/logfile'),
          ('lld-logfile-0.17', LLD + 'mirror/meta/0.17/logfile'),
          ('cszo-logfile-0.17', CSZO + 'meta/0.17/logfile')]
 # this line should be used on CSZO instead:
 #         'cszo-logfile-0.17']
 TEST_MILESTONES = USE_TEST and [
          ('cao-milestones-0.17', CAO + 'milestones17'),
-         ('cdo-milestones-0.17', CDO + 'milestones-0.17.txt'),
-         ('cue-milestones-0.17', CUE + 'meta/0.17/milestones'),
+         #('cdo-milestones-0.17', CDO + 'milestones-0.17.txt'),
+         #('cue-milestones-0.17', CUE + 'meta/0.17/milestones'),
          ('cbro-milestones-0.17', CBRO + 'meta/0.17/milestones'),
-         ('cpo-milestones-0.17', CPO + 'dcss-milestones-0.17'),
-         ('cwz-milestones-0.17', CWZ + '0.17/milestones'),
-         ('cxc-milestones-0.17', CXC + 'meta/0.17/milestones'),
+         #('cpo-milestones-0.17', CPO + 'dcss-milestones-0.17'),
+         #('cwz-milestones-0.17', CWZ + '0.17/milestones'),
+         #('cxc-milestones-0.17', CXC + 'meta/0.17/milestones'),
          ('lld-milestones-0.17', LLD + 'mirror/meta/0.17/milestones'),
          ('cszo-milestones-0.17', CSZO + 'meta/0.17/milestones')]
 # this line should be used on CSZO instead:
