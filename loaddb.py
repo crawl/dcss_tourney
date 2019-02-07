7b52341806785d0177970289e2859aadf83f46f4
gammafunk <gammafunk@gmail.com>
2019-02-07 15:01:19 -0600

    Enable 0.23 tournament entries for CDO and CWZ

--- loaddb.py
+++ loaddb.py
@@ -48,11 +48,11 @@ LLD = 'http://lazy-life.ddo.jp/'
 LOGS = TEST_LOGS or [
 #          ('cao-logfile-0.23', CAO + 'logfile23'),
 #          ('cbro-logfile-0.23', CBRO + 'meta/0.23/logfile'),
-#          ('cdo-logfile-0.23', CDO + 'allgames-0.23.txt'),
+           ('cdo-logfile-0.23', CDO + 'allgames-0.23.txt'),
            ('cko-logfile-0.23', CKO + 'meta/0.23/logfile'),
            ('cpo-logfile-0.23', CPO + 'dcss-logfiles-0.23'),
            ('cue-logfile-0.23', CUE + 'meta/0.23/logfile'),
-#          ('cwz-logfile-0.23', CWZ + '0.23/logfile'),
+           ('cwz-logfile-0.23', CWZ + '0.23/logfile'),
            ('cxc-logfile-0.23', CXC + 'meta/0.23/logfile'),
            ('lld-logfile-0.23', LLD + 'mirror/meta/0.23/logfile'),
   ]
@@ -60,11 +60,11 @@ LOGS = TEST_LOGS or [
 MILESTONES = TEST_MILESTONES or [
 #          ('cao-milestones-0.23', CAO + 'milestones23'),
 #          ('cbro-milestones-0.23', CBRO + 'meta/0.23/milestones'),
-#          ('cdo-milestones-0.23', CDO + 'milestones-0.23.txt'),
+           ('cdo-milestones-0.23', CDO + 'milestones-0.23.txt'),
            ('cko-milestones-0.23', CKO + 'meta/0.23/milestones'),
            ('cpo-milestones-0.23', CPO + 'dcss-milestones-0.23'),
            ('cue-milestones-0.23', CUE + 'meta/0.23/milestones'),
-#          ('cwz-milestones-0.23', CWZ + '0.23/milestones'),
+           ('cwz-milestones-0.23', CWZ + '0.23/milestones'),
            ('cxc-milestones-0.23', CXC + 'meta/0.23/milestones'),
            ('lld-milestones-0.23', LLD + 'mirror/meta/0.23/milestones'),
   ]
--- test_data.py
+++ test_data.py
@@ -22,11 +22,11 @@ TEST_HARE_START_TIME = USE_TEST and (TEST_YEAR + '02052000')
 TEST_LOGS = USE_TEST and [
 #          ('cao-logfile-0.23', CAO + 'logfile23'),
 #          ('cbro-logfile-0.23', CBRO + 'meta/0.23/logfile'),
-#          ('cdo-logfile-0.23', CDO + 'allgames-0.23.txt'),
+           ('cdo-logfile-0.23', CDO + 'allgames-0.23.txt'),
            ('cko-logfile-0.23', CKO + 'meta/0.23/logfile'),
            ('cpo-logfile-0.23', CPO + 'dcss-logfiles-0.23'),
            ('cue-logfile-0.23', CUE + 'meta/0.23/logfile'),
-#          ('cwz-logfile-0.23', CWZ + '0.23/logfile'),
+           ('cwz-logfile-0.23', CWZ + '0.23/logfile'),
            ('cxc-logfile-0.23', CXC + 'meta/0.23/logfile'),
            ('lld-logfile-0.23', LLD + 'mirror/meta/0.23/logfile'),
   ]
@@ -34,11 +34,11 @@ TEST_LOGS = USE_TEST and [
 TEST_MILESTONES = USE_TEST and [
 #          ('cao-milestones-0.23', CAO + 'milestones23'),
 #          ('cbro-milestones-0.23', CBRO + 'meta/0.23/milestones'),
-#          ('cdo-milestones-0.23', CDO + 'milestones-0.23.txt'),
+           ('cdo-milestones-0.23', CDO + 'milestones-0.23.txt'),
            ('cko-milestones-0.23', CKO + 'meta/0.23/milestones'),
            ('cpo-milestones-0.23', CPO + 'dcss-milestones-0.23'),
            ('cue-milestones-0.23', CUE + 'meta/0.23/milestones'),
-#          ('cwz-milestones-0.23', CWZ + '0.23/milestones'),
+           ('cwz-milestones-0.23', CWZ + '0.23/milestones'),
            ('cxc-milestones-0.23', CXC + 'meta/0.23/milestones'),
            ('lld-milestones-0.23', LLD + 'mirror/meta/0.23/milestones'),
   ]
