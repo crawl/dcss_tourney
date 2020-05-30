c66842228ec197e82d171478a4b72b11cea9669a
Alex Jurkiewicz <alex@jurkiewi.cz>
2020-05-30 10:14:40 +1000

    Move Query to its own file
    This is needed to support some refactoring in the next few commits.

--- loaddb.py
+++ loaddb.py
@@ -24,6 +24,7 @@ except:
 import imp
 import sys
 
+from query_class import Query
 from test_data import USE_TEST, TEST_YEAR, TEST_VERSION, TEST_START_TIME, TEST_END_TIME, TEST_HARE_START_TIME, TEST_LOGS, TEST_MILESTONES, TEST_CLAN_DEADLINE, LogSpec
 
 T_YEAR = TEST_YEAR or '2019'
@@ -691,46 +692,6 @@ def fix_crawl_date(date):
     return "%s%02d%s" % (match.group(1), 1 + int(match.group(2)),
                          match.group(3))
   return R_MONTH_FIX.sub(inc_month, date)
-
-class Query:
-  def __init__(self, qstring, *values):
-    self.query = qstring
-    self.values = values
-
-  def append(self, qseg, *values):
-    self.query += qseg
-    self.values += values
-
-  def vappend(self, *values):
-    self.values += values
-
-  def execute(self, cursor):
-    """Executes query on the supplied cursor."""
-    self.query = self.query.strip()
-    if not self.query.endswith(';'):
-      self.query += ';'
-    cursor.execute(self.query, self.values)
-
-  def row(self, cursor):
-    """Executes query and returns the first row tuple, or None if there are no
-    rows."""
-    self.execute(cursor)
-    return cursor.fetchone()
-
-  def rows(self, cursor):
-    self.execute(cursor)
-    return cursor.fetchall()
-
-  def count(self, cursor, msg=None, exc=Exception):
-    """Executes a SELECT COUNT(foo) query and returns the count. If there is
-    not at least one row, raises an exception."""
-    self.execute(cursor)
-    row = cursor.fetchone()
-    if row is None:
-      raise exc(msg or "No rows returned for %s" % self.query)
-    return row[0]
-
-  first = count
 
 char = SqlType(lambda x: x)
 #remove the trailing 'D'/'S', fixup date
