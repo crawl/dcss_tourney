4258d163d1eec7b6aef810b4b034ab180cfdc180
gammafunk <gammafunk@gmail.com>
2021-01-05 05:23:12 -0600

    Fix the test data sources
    I forgot that we eventually stopped fetching these and only used a local
    copy of some of the data. We may eventually do that in our tests, but
    for now we actually want to fetch the data in realtime.

--- test_data.py
+++ test_data.py
@@ -27,25 +27,25 @@ TEST_HARE_START_TIME = USE_TEST and (TEST_YEAR + '01072000')
 LogSpec = collections.namedtuple('LogSpec', ('src', 'local_path', 'url'))
 
 TEST_LOGS = USE_TEST and [
-#           LogSpec('cao', 'logfiles/cao-logfile-0.26', None),  # CAO + 'logfile26'),
-            LogSpec('cbr2', 'logfiles/cbr2-logfile-0.26', None),  # CBR2 + 'meta/0.26/logfile'),
-#           LogSpec('cdo', 'logfiles/cdo-logfile-0.26', None),  # CDO + 'allgames-0.26.txt'),
-            LogSpec('cko', 'logfiles/cko-logfile-0.26', None),  # CKO + 'meta/0.26/logfile'),
-#           LogSpec('cpo', 'logfiles/cpo-logfile-0.26', None),  # CPO + 'dcss-logfiles-0.26'),
-            LogSpec('cue', 'logfiles/cue-logfile-0.26', None),  # CUE + 'meta/0.26/logfile'),
-#           LogSpec('cwz', 'logfiles/cwz-logfile-0.26', None),  # CWZ + '0.26/logfile'),
-            LogSpec('cxc', 'logfiles/cxc-logfile-0.26', None),  # CXC + 'meta/0.26/logfile'),
-#           LogSpec('lld', 'logfiles/lld-logfile-0.26', None),  # LLD + 'mirror/meta/0.26/logfile'),
+#           LogSpec('cao', 'logfiles/cao-logfile-0.26', CAO + 'logfile26'),
+            LogSpec('cbr2', 'logfiles/cbr2-logfile-0.26', CBR2 + 'meta/0.26/logfile'),
+#           LogSpec('cdo', 'logfiles/cdo-logfile-0.26', CDO + 'allgames-0.26.txt'),
+            LogSpec('cko', 'logfiles/cko-logfile-0.26', CKO + 'meta/0.26/logfile'),
+#           LogSpec('cpo', 'logfiles/cpo-logfile-0.26', CPO + 'dcss-logfiles-0.26'),
+            LogSpec('cue', 'logfiles/cue-logfile-0.26', CUE + 'meta/0.26/logfile'),
+#           LogSpec('cwz', 'logfiles/cwz-logfile-0.26', CWZ + '0.26/logfile'),
+            LogSpec('cxc', 'logfiles/cxc-logfile-0.26', CXC + 'meta/0.26/logfile'),
+#           LogSpec('lld', 'logfiles/lld-logfile-0.26', LLD + 'mirror/meta/0.26/logfile'),
   ]
 
 TEST_MILESTONES = USE_TEST and [
-#           LogSpec('cao', 'milestones/cao-milestones-0.26', None),  # CAO + 'milestones26'),
-            LogSpec('cbr2', 'milestones/cbr2-milestones-0.26', None),  # CBR2 + 'meta/0.26/milestones'),
-#           LogSpec('cdo', 'milestones/cdo-milestones-0.26', None),  # CDO + 'milestones-0.26.txt'),
-            LogSpec('cko', 'milestones/cko-milestones-0.26', None),  # CKO + 'meta/0.26/milestones'),
-#           LogSpec('cpo', 'milestones/cpo-milestones-0.26', None),  # CPO + 'dcss-milestones-0.26'),
-            LogSpec('cue', 'milestones/cue-milestones-0.26', None),  # CUE + 'meta/0.26/milestones'),
-#           LogSpec('cwz', 'milestones/cwz-milestones-0.26', None),  # CWZ + '0.26/milestones'),
-            LogSpec('cxc', 'milestones/cxc-milestones-0.26', None),  # CXC + 'meta/0.26/milestones'),
-#           LogSpec('lld', 'milestones/lld-milestones-0.26', None),  # LLD + 'mirror/meta/0.26/milestones'),
+#           LogSpec('cao', 'milestones/cao-milestones-0.26', CAO + 'milestones26'),
+            LogSpec('cbr2', 'milestones/cbr2-milestones-0.26', CBR2 + 'meta/0.26/milestones'),
+#           LogSpec('cdo', 'milestones/cdo-milestones-0.26', CDO + 'milestones-0.26.txt'),
+            LogSpec('cko', 'milestones/cko-milestones-0.26', CKO + 'meta/0.26/milestones'),
+#           LogSpec('cpo', 'milestones/cpo-milestones-0.26', CPO + 'dcss-milestones-0.26'),
+            LogSpec('cue', 'milestones/cue-milestones-0.26', CUE + 'meta/0.26/milestones'),
+#           LogSpec('cwz', 'milestones/cwz-milestones-0.26', CWZ + '0.26/milestones'),
+            LogSpec('cxc', 'milestones/cxc-milestones-0.26', CXC + 'meta/0.26/milestones'),
+#           LogSpec('lld', 'milestones/lld-milestones-0.26', LLD + 'mirror/meta/0.26/milestones'),
   ]
