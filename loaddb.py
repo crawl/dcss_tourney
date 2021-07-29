10818711f760b9daf66229552a9cd77a17eb20e4
gammafunk <gammafunk@gmail.com>
2021-07-29 16:10:43 -0500

    feat: Add CWZ to the 0.27 sources

--- loaddb.py
+++ loaddb.py
@@ -63,7 +63,7 @@ LOGS = TEST_LOGS or [
             LogSpec('cko', 'logfiles/cko-logfile-0.27', CKO + 'meta/0.27/logfile'),
             LogSpec('cpo', 'logfiles/cpo-logfile-0.27', CPO + 'dcss-logfiles-0.27'),
             LogSpec('cue', 'logfiles/cue-logfile-0.27', CUE + 'meta/0.27/logfile'),
-#           LogSpec('cwz', 'logfiles/cwz-logfile-0.27', CWZ + '0.27/logfile'),
+            LogSpec('cwz', 'logfiles/cwz-logfile-0.27', CWZ + '0.27/logfile'),
             LogSpec('cxc', 'logfiles/cxc-logfile-0.27', CXC + 'meta/0.27/logfile'),
             LogSpec('lld', 'logfiles/lld-logfile-0.27', LLD + 'mirror/meta/0.27/logfile'),
   ]
@@ -75,7 +75,7 @@ MILESTONES = TEST_MILESTONES or [
             LogSpec('cko', 'milestones/cko-milestones-0.27', CKO + 'meta/0.27/milestones'),
             LogSpec('cpo', 'milestones/cpo-milestones-0.27', CPO + 'dcss-milestones-0.27'),
             LogSpec('cue', 'milestones/cue-milestones-0.27', CUE + 'meta/0.27/milestones'),
-#           LogSpec('cwz', 'milestones/cwz-milestones-0.27', CWZ + '0.27/milestones'),
+            LogSpec('cwz', 'milestones/cwz-milestones-0.27', CWZ + '0.27/milestones'),
             LogSpec('cxc', 'milestones/cxc-milestones-0.27', CXC + 'meta/0.27/milestones'),
             LogSpec('lld', 'milestones/lld-milestones-0.27', LLD + 'mirror/meta/0.27/milestones'),
   ]
